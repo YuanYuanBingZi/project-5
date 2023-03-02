85e86f3d511eb6016977e028deee0218a6131a5d
Tong Guan <yuanyuanbingzi@gmail.com>
2023-03-02 14:09:57 -0800

    fixed the error on the test file

--- brevets/tests/test_db.py
+++ brevets/tests/test_db.py
@@ -13,17 +13,44 @@ log = logging.getLogger(__name__)
 def test_db_insertion():
     start_time = arrow.get("2023-02-17 00:00", "YYYY-MM-DD HH:mm")
     dist = 200
-    checkpoints = {
-        0: (start_time, start_time.shift(hours = 1)),
-        50: (start_time.shift(hours = 1, minutes = 28), start_time.shift(hours = 3, minutes = 30)),
-        150:(start_time.shift(hours = 4, minutes = 25), start_time.shift(hours = 10)),
-        200:(start_time.shift(hours = 5, minutes = 53), start_time.shift(hours = 13, minutes = 30))
-    }
+    checkpoints = [
+        {"km": 0, "open_time": start_time, "close_time":start_time.shift(hours = 1)},
+        {"km": 50, "open_time": start_time.shift(hours = 1, minutes = 28), "close_time":start_time.shift(hours = 3, minutes = 30)},
+        {"km": 150, "open_time": start_time.shift(hours = 4, minutes = 25), "close_time":start_time.shift(hours = 10)},
+        {"km": 200, "open_time": start_time.shift(hours = 5, minutes = 53), "close_time":start_time.shift(hours = 13, minutes = 30)}
+    ]
+
+    for checkpoint in checkpoints:
+        checkpoint["open_time"] = checkpoint["open_time"].format('YYYY-MM-DDTHH:mm')
+        checkpoint["close_time"] = checkpoint["close_time"].format('YYYY-MM-DDTHH:mm')
+
+    start_time = start_time.format('YYYY-MM-DDTHH:mm')
 
     id = insert_brevet(dist, start_time, checkpoints)
-    new_start_time, new_dist, new_checkpoints = get_brevet()
+    new_dist, new_start_time, new_checkpoints = get_brevet()
     assert new_start_time == start_time
     assert new_dist == dist
     assert new_checkpoints == checkpoints
 
-    
+
+def test_db_get_brevets():
+    start_time = arrow.get("2023-02-17 00:00", "YYYY-MM-DD HH:mm")
+    dist = 400
+    checkpoints = [
+        {"km": 0, "open_time": start_time, "close_time":start_time.shift(hours = 1)},
+        {"km": 50, "open_time": start_time.shift(hours = 1, minutes = 28), "close_time":start_time.shift(hours = 3, minutes = 30)},
+        {"km": 320, "open_time": start_time.shift(hours = 9, minutes = 38), "close_time":start_time.shift(hours = 21, minutes = 20)},
+        {"km": 400, "open_time": start_time.shift(hours = 12, minutes = 8), "close_time":start_time.shift(hours = 27)}
+    ]
+
+    for checkpoint in checkpoints:
+        checkpoint["open_time"] = checkpoint["open_time"].format('YYYY-MM-DDTHH:mm')
+        checkpoint["close_time"] = checkpoint["close_time"].format('YYYY-MM-DDTHH:mm')
+
+    start_time = start_time.format('YYYY-MM-DDTHH:mm')
+
+    id = insert_brevet(dist, start_time, checkpoints)
+    new_dist, new_start_time, new_checkpoints = get_brevet()
+    assert new_start_time == start_time
+    assert new_dist == dist
+    assert new_checkpoints == checkpoints
